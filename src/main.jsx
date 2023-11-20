546d5577908a4cc31b6a73d0609c19d61d404bbd
SereyvatanaUng <sereyvatanau@gmail.com>
2023-11-20 19:40:47 +0700

    createAsyncThunk

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice";
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
