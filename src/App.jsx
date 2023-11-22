4f3d90af913aeb462d82e5512c65cc712d843502
SereyvatanaUng <sereyvatanau@gmail.com>
2023-11-22 14:14:04 +0700

    CRUD example

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import AddPostForm from "./features/posts/AddPostForm";
 import PostsList from "./features/posts/PostsList";
+import EditPostForm from "./features/posts/EditPostForm";
+import Layout from "./components/Layout";
+import SinglePostPage from "./features/posts/SinglePostPage";
 
 function App() {
   return (
-    <main className="App">
-      <AddPostForm />
-      <PostsList />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path=":edit/:postId" element={<EditPostForm />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,20 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { fetchUsers } from "./features/users/usersSlice";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { fetchPosts } from "./features/posts/postsSlice";
+
+store.dispatch(fetchPosts());
 store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
